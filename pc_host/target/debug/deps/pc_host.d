fab546b36c936b2fdee7936c1abf8dc3dd8ce52d
Jih15 <99134563+Jih15@users.noreply.github.com>
2025-12-03 16:29:22 +0700

    refactor: add bluetooth module and fix mobile

--- pc_host/target/debug/deps/pc_host.d
+++ pc_host/target/debug/deps/pc_host.d
@@ -1,10 +1,11 @@
-D:\My-Project\flutter\mobile_pc_controller\pc_host\target\debug\deps\pc_host.d: src\main.rs src\network\mod.rs src\network\websocket.rs src\input\mod.rs src\input\controller_event.rs src\input\vigem_mapper.rs src\controller\mod.rs src\controller\state.rs src\utils\mod.rs src\utils\logger.rs src\utils\config.rs
+D:\My-Project\flutter\mobile_pc_controller\pc_host\target\debug\deps\pc_host.d: src\main.rs src\network\mod.rs src\network\websocket.rs src\network\bluetooth.rs src\input\mod.rs src\input\controller_event.rs src\input\vigem_mapper.rs src\controller\mod.rs src\controller\state.rs src\utils\mod.rs src\utils\logger.rs src\utils\config.rs
 
-D:\My-Project\flutter\mobile_pc_controller\pc_host\target\debug\deps\pc_host.exe: src\main.rs src\network\mod.rs src\network\websocket.rs src\input\mod.rs src\input\controller_event.rs src\input\vigem_mapper.rs src\controller\mod.rs src\controller\state.rs src\utils\mod.rs src\utils\logger.rs src\utils\config.rs
+D:\My-Project\flutter\mobile_pc_controller\pc_host\target\debug\deps\pc_host.exe: src\main.rs src\network\mod.rs src\network\websocket.rs src\network\bluetooth.rs src\input\mod.rs src\input\controller_event.rs src\input\vigem_mapper.rs src\controller\mod.rs src\controller\state.rs src\utils\mod.rs src\utils\logger.rs src\utils\config.rs
 
 src\main.rs:
 src\network\mod.rs:
 src\network\websocket.rs:
+src\network\bluetooth.rs:
 src\input\mod.rs:
 src\input\controller_event.rs:
 src\input\vigem_mapper.rs:
